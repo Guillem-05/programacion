29eb57e2c351af4e0e3f967c38b2018f170aa550
Guillem-05 <gusall@alumnatflorida.es>
2024-12-17 08:41:17 +0100

    Add files via upload

---
+++ BaseProgramacionXdebug1DAW-main/public/evaluable/index.php
@@ -0,0 +1,254 @@
+<?php
+$pomodoroHaters = [
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', 'A', 'A', 'A', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '0', '0', '0', 'A', '0', 'A', 'A', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', 'A', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', 'A', 'A', '0', '0', 'A', '0', '0', '0', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', 'A', 'A', '0', '0', 'A', '0', '0', '0', 'A', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', '0', '0', '0', 'A', '0', 'A', '0', '0', '0', '~'],
+    ['~', '~', '~', '0', 'A', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '0', '0', '0', 'A', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '0', '0', '0', '0', 'A', '0', '0', '0', '0', 'A', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '0', '0', '0', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '0', '0', '0', '0', '0', 'A', '0', '0', '0', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~'],
+    ['~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~', '~']
+];
+$impacts = [
+    [20, 4],
+    [2, 13],
+    [13, 12],
+    [3, 17],
+    [2, 13],
+    [5, 19],
+    [6, 18],
+    [5, 2],
+    [20, 13],
+    [9, 7],
+    [5, 9],
+    [15, 16],
+    [16, 13],
+    [16, 9],
+    [16, 0],
+    [3, 19],
+    [19, 8],
+    [1, 16],
+    [18, 4],
+    [21, 23],
+    [7, 17],
+    [22, 15],
+    [21, 6],
+    [14, 8],
+    [12, 23],
+    [7, 7],
+    [22, 4],
+    [3, 21],
+    [2, 3],
+    [8, 11],
+    [0, 4],
+    [7, 21],
+    [11, 4],
+    [7, 15],
+    [6, 17],
+    [5, 19],
+    [4, 19],
+    [4, 7],
+    [23, 21],
+    [15, 20],
+    [2, 9],
+    [21, 2],
+    [1, 13],
+    [7, 10],
+    [21, 5],
+    [13, 17],
+    [2, 14],
+    [11, 14],
+    [22, 17],
+    [18, 22],
+    [2, 23],
+    [3, 1],
+    [18, 6],
+    [17, 12],
+    [18, 18],
+    [20, 2],
+    [3, 14],
+    [11, 21],
+    [6, 5],
+    [6, 2],
+    [12, 23],
+    [18, 18],
+    [21, 6],
+    [10, 12],
+    [5, 4],
+    [16, 19],
+    [8, 10],
+    [12, 21],
+    [15, 1],
+    [20, 14],
+    [3, 20],
+    [6, 19],
+    [20, 13],
+    [15, 4],
+    [10, 2],
+    [5, 16],
+    [20, 1],
+    [12, 13],
+    [11, 5],
+    [12, 14],
+    [8, 3],
+    [6, 8],
+    [19, 7],
+    [16, 9],
+    [13, 20],
+    [3, 5],
+    [1, 0],
+    [20, 14],
+    [12, 21],
+    [12, 16],
+    [15, 7],
+    [9, 1],
+    [1, 18],
+    [20, 6],
+    [8, 6],
+    [22, 1],
+    [10, 22],
+    [19, 19],
+    [7, 16],
+    [8, 8]
+];
+//ESCRIBE AQUÍ TU PROGRAMA PRINCIPAL
+
+// Define una función para imprimir el mapa de Pomodoro
+function mostrarMapa($mapa) {
+    foreach ($mapa as $fila) {
+        echo implode(' ', $fila) . "<br>";
+    }
+}
+echo "MAPA ORIGINAL<br>";
+mostrarMapa($pomodoroHaters);
+
+function marcarNucleosUrbanosAfectados($mapa, $impactos) {
+    $mapaModificado = $mapa;
+    foreach ($impactos as $impacto) {
+        $fila = $impacto[0];
+        $columna = $impacto[1];
+        if ($mapaModificado[$fila][$columna] == 'A') {
+            $mapaModificado[$fila][$columna] = 'C';
+        }
+    }
+    return $mapaModificado;
+}
+echo "MAPA CON IMPACTOS URBANOS <br>";
+$mapaModificado = marcarNucleosUrbanosAfectados($pomodoroHaters, $impacts);
+mostrarMapa($mapaModificado);
+function estimarpersonas($mapa) {
+    $colirios = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda == 'C') {
+                $colirios +=5000;
+            }
+        }
+    }
+    $personasAfect = $colirios ;
+    return $personasAfect;
+}
+
+$personasAfect = estimarpersonas($mapaModificado);
+echo "UN TOTAL DE " . $personasAfect . "  DE PERSONAS AFECTADAS <br>";
+
+function estimarColirioNecesario($mapa) {
+    $personasAfectadas = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda == 'C') {
+                $personasAfectadas += 5000;
+            }
+        }
+    }
+    $litrosColirioNecesarios = $personasAfectadas * 25 / 1000;
+    return $litrosColirioNecesarios;
+}
+
+$litrosColirioNecesarios = estimarColirioNecesario($mapaModificado);
+echo "SE NECESITA UN TOTAL DE " . $litrosColirioNecesarios . " DE COLIRIO <br>";
+echo "<br>";
+echo "MAPA CON IMPACTOS URBANOS, NO URBANOS Y MAR <br>";
+
+
+function marcarImpactos($mapa, $impactos) {
+    $mapaModificado = $mapa;
+    foreach ($impactos as $impacto) {
+        $fila = $impacto[0];
+        $columna = $impacto[1];
+        if ($mapaModificado[$fila][$columna] == '~') {
+            $mapaModificado[$fila][$columna] = 'S';
+        } elseif ($mapaModificado[$fila][$columna] == '0') {
+            $mapaModificado[$fila][$columna] = 'X';
+        }
+    }
+    return $mapaModificado;
+}
+
+$mapaModificado = marcarImpactos($mapaModificado, $impacts);
+mostrarMapa($mapaModificado);
+
+
+function estimarDanosEconomicos($mapa) {
+    $danosEconomicos = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda == 'X') {
+                $danosEconomicos += 50000;
+            } elseif ($celda == 'C') {
+                $danosEconomicos += 200000;
+            }
+        }
+    }
+    return $danosEconomicos;
+}
+
+$danosEconomicos = estimarDanosEconomicos($mapaModificado);
+echo "Daños económicos: " . $danosEconomicos . "<br>";
+
+
+function estimarPescadoDisponible($mapa) {
+    $pescadoDisponible = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda == '~') {
+                $pescadoDisponible += 2000 / count($mapa);
+            }
+        }
+    }
+    return $pescadoDisponible;
+}
+
+$pescadoDisponible = estimarPescadoDisponible($mapaModificado);
+echo "Pescado disponible: " . $pescadoDisponible . "<br>";
+
+
+function estimarPescadoAfectado($mapa) {
+    $pescadoAfectado = 0;
+    foreach ($mapa as $fila) {
+        foreach ($fila as $celda) {
+            if ($celda == 'S') {
+                $pescadoAfectado += 2000 / count($mapa);
+            }
+        }
+    }
+    return $pescadoAfectado;
+}
+//ESCRIBE AQUÍ LA DEFINICIÓN DE LAS FUNCIONES
+?>
